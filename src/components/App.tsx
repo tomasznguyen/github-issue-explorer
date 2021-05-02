2c0a5b197e02482551f2c0a4caf32785f07b5641
Tomasz Nguyen <tomasznguyen@gmail.com>
2021-05-03 00:35:52 +0200

    Add global theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import usePagination from "../hooks/usePagination";
+import { fetchIssues, selectIssues } from "../redux/issuesSlice";
+import { useAppDispatch } from "../redux/store";
+import GlobalStyle from "../theme/GlobalStyle";
+import IssueForm from "./IssueForm";
+import IssueList from "./IssueList";
+import Pagination from "./Pagination";
 
-function App() {
-  return <div></div>;
-}
+export const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const issues = useSelector(selectIssues);
+  const { currentPage, pageItems, pageCount, setCurrentPage } = usePagination(
+    issues,
+    10
+  );
+
+  const handleSubmit = async (organization: string, repository: string) => {
+    dispatch(fetchIssues({ organization, repository }));
+  };
+
+  return (
+    <>
+      <GlobalStyle />
+      <IssueForm onSubmit={handleSubmit} />
+      <IssueList issues={pageItems} />
+      <Pagination
+        page={currentPage}
+        pages={pageCount}
+        onPageChange={setCurrentPage}
+      />
+    </>
+  );
+};
 
 export default App;
