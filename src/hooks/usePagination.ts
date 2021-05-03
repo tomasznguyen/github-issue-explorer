e0e83caeacfec158a981a593b506f874160073a8
Tomasz Nguyen <tomasznguyen@gmail.com>
2021-05-03 07:44:53 +0200

    Add comment

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,5 +1,14 @@
 import React from "react";
 
+/**
+ * Computes the necessary information for a pagination component based on
+ * a list of items and the current page.
+ * @param {Array} items - The items to be paginated
+ * @param {number} itemsPerPage - The max number of items per page
+ * @returns {Object} - An object containing the current, the items of the
+ *                     current page, the total number of pages and a function
+ *                     to change the current page
+ */
 function usePagination<T>(items: T[], itemsPerPage: number = 25) {
   const [currentPage, setCurrentPage] = React.useState(0);
 
