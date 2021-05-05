1b80bf23efdd85b6c4e7bd98694600136f76c550
Tomasz Nguyen <tomasznguyen@gmail.com>
2021-05-05 02:53:41 +0200

    Add api calls to fetch issue and pull request count

--- src/api/github.ts
+++ src/api/github.ts
@@ -10,10 +10,63 @@ import { Issue } from "../types";
 export const fetchIssues = (
   organization: string,
   repository: string,
-  page: number = 1
+  issuesPerPage: number,
+  page: number
 ): Promise<Issue[]> => {
   return fetch(
-    `https://api.github.com/repos/${organization}/${repository}/issues?per_page=100&page=${page}&state=all`
+    `https://api.github.com/repos/${organization}/${repository}/issues?per_page=${issuesPerPage}&page=${page}&state=all`
+  )
+    .then(async (res) => {
+      if (res.status >= 400) {
+        return Promise.reject(await res.json());
+      }
+
+      return res.json();
+    })
+    .catch((err) => Promise.reject<string>(err.message));
+};
+
+/**
+ * Searches GitHub issues for a given organization and repository
+ * @param {string} organization - An organization on GitHub
+ * @param {string} repository - An organization's repository
+ * @param {number} page - Page number of the results to fetch
+ * @returns {Object} - A list of issues and the total issue count
+ */
+export const searchIssues = (
+  organization: string,
+  repository: string,
+  issuesPerPage: number,
+  page: number
+): Promise<{ total_count: number; items: Issue[] }> => {
+  return fetch(
+    `https://api.github.com/search/issues?q=repo:${organization}/${repository}+type:issue&page=${page}&per_page=${issuesPerPage}`
+  )
+    .then(async (res) => {
+      if (res.status >= 400) {
+        return Promise.reject(await res.json());
+      }
+
+      return res.json();
+    })
+    .catch((err) => Promise.reject<string>(err.message));
+};
+
+/**
+ * Searches GitHub pull requests for a given organization and repository
+ * @param {string} organization - An organization on GitHub
+ * @param {string} repository - An organization's repository
+ * @param {number} page - Page number of the results to fetch
+ * @returns {Object} - A list of pull requests and the total pull request count
+ */
+export const searchPulls = (
+  organization: string,
+  repository: string,
+  issuesPerPage: number,
+  page: number
+): Promise<{ total_count: number; items: Issue[] }> => {
+  return fetch(
+    `https://api.github.com/search/issues?q=repo:${organization}/${repository}+type:pull-request&page=${page}&per_page=${issuesPerPage}`
   )
     .then(async (res) => {
       if (res.status >= 400) {
